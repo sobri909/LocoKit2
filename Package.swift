c595c2be1f04d1a9d9735db8d5a28fe3eb66da4c
Matt <matt@bigpaua.com>
2024-03-11 11:19:47 +0530

    this structure seems to work better / cleaner?

--- Package.swift
+++ Package.swift
@@ -13,19 +13,12 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Jounce/Surge", from: "2.3.0"),
-        .package(url: "https://github.com/apple/swift-log", from: "1.5.4"),
-        .package(url: "https://github.com/Adorkable/swift-log-format-and-pipe", from: "0.1.1"),
-        .package(url: "https://github.com/groue/GRDB.swift.git", from: "6.25.0"),
+        .package(name: "Logging", url: "https://github.com/apple/swift-log", from: "1.5.4"),
+        .package(name: "LoggingFormatAndPipe", url: "https://github.com/Adorkable/swift-log-format-and-pipe", from: "0.1.1"),
+        .package(name: "GRDB", url: "https://github.com/groue/GRDB.swift", from: "6.25.0"),
     ],
     targets: [
-        .target(
-            name: "LocoKit2",
-            dependencies: [
-                "Surge", "GRDB",
-                .product(name: "Logging", package: "swift-log"),
-                .product(name: "LoggingFormatAndPipe", package: "swift-log-format-and-pipe")
-            ]
-        ),
+        .target(name: "LocoKit2", dependencies: ["Surge", "GRDB", "Logging", "LoggingFormatAndPipe"]),
         .testTarget(name: "LocoKit2Tests", dependencies: ["LocoKit2"])
     ]
 )
