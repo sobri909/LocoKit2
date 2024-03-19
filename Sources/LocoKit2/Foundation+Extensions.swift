7d38f83a7d06f13a3f2c75202e9316eff92ccff8
Matt <matt@bigpaua.com>
2024-03-19 15:41:13 +0530

    misc convenience

--- Sources/LocoKit2/Foundation+Extensions.swift
+++ Sources/LocoKit2/Foundation+Extensions.swift
@@ -4,10 +4,6 @@
 
 import Foundation
 
-extension Date {
-    var age: TimeInterval { -timeIntervalSinceNow }
-}
-
 extension String {
     func appendLineTo(_ url: URL) throws {
         try (self + "\n").appendTo(url)
@@ -59,4 +55,41 @@ func withContinousObservation<T>(of value: @escaping @autoclosure () -> T, execu
             withContinousObservation(of: value(), execute: execute)
         }
     }
+}
+
+extension Date {
+    var age: TimeInterval { return -timeIntervalSinceNow }
+
+    func isToday(in calendar: Calendar = Calendar.current) -> Bool { calendar.isDateInToday(self) }
+    func isYesterday(in calendar: Calendar = Calendar.current) -> Bool { calendar.isDateInYesterday(self) }
+    func isTomorrow(in calendar: Calendar = Calendar.current) -> Bool { calendar.isDateInTomorrow(self) }
+
+    func startOfDay(in calendar: Calendar = Calendar.current) -> Date { calendar.startOfDay(for: self) }
+    func middleOfDay(in calendar: Calendar = Calendar.current) -> Date { startOfDay(in: calendar) + .hours(12) } // approximation
+    func endOfDay(in calendar: Calendar = Calendar.current) -> Date { nextDay(in: calendar).startOfDay(in: calendar) }
+
+    func sinceStartOfDay(in calendar: Calendar = Calendar.current) -> TimeInterval { timeIntervalSince(startOfDay(in: calendar)) }
+    func dayOfMonth(in calendar: Calendar = Calendar.current) -> Int { calendar.component(.day, from: self) }
+
+    func nextDay(in calendar: Calendar = Calendar.current) -> Date { calendar.nextDay(from: self) }
+    func previousDay(in calendar: Calendar = Calendar.current) -> Date { calendar.previousDay(from: self) }
+
+    func subtracting(days: Int, in calendar: Calendar = Calendar.current) -> Date { calendar.date(byAdding: .day, value: -days, to: self)! }
+    func adding(days: Int, in calendar: Calendar = Calendar.current) -> Date { calendar.date(byAdding: .day, value: days, to: self)! }
+
+    func isSameDayAs(_ date: Date, in calendar: Calendar = Calendar.current) -> Bool { calendar.isDate(date, inSameDayAs: self) }
+    func isSameMonthAs(_ date: Date, in calendar: Calendar = Calendar.current) -> Bool { calendar.isDate(date, equalTo: self, toGranularity: .month) }
+
+    static func -(lhs: Date, rhs: Date) -> TimeInterval {
+        return lhs.timeIntervalSince(rhs)
+    }
+}
+
+extension Calendar {
+    func previousDay(from date: Date) -> Date { self.date(byAdding: .day, value: -1, to: date)! }
+    func nextDay(from date: Date) -> Date { self.date(byAdding: .day, value: 1, to: date)! }
+}
+
+extension DateInterval {
+    var range: ClosedRange<Date> { start...end }
 }
