eaa96a442652688f2cf448d5ca7e3d8bd79c609f
Matt <matt@bigpaua.com>
2024-03-19 16:42:41 +0530

    sometimes need equatable n friends

--- Sources/LocoKit2/Models/TimelineItem.swift
+++ Sources/LocoKit2/Models/TimelineItem.swift
@@ -8,11 +8,19 @@
 import Foundation
 import GRDB
 
-public struct TimelineItem: Identifiable, Decodable, FetchableRecord {
+public struct TimelineItem: FetchableRecord, Decodable, Identifiable, Hashable {
     public let base: TimelineItemBase
     public let visit: TimelineItemVisit?
     public let trip: TimelineItemTrip?
     public let samples: [LocomotionSample]
 
     public var id: String { base.id }
+
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+
+    public static func == (lhs: TimelineItem, rhs: TimelineItem) -> Bool {
+        return lhs.id == rhs.id
+    }
 }
