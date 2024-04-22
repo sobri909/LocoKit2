7fa47a9440c2aa1125262bf11792e5d526340b1d
Matt <matt@bigpaua.com>
2024-04-22 14:43:41 +0800

    added memory footprint extension, and moved Calendar extensions to separate file

---
+++ Sources/LocoKit2/Extensions/Calendar.swift
@@ -0,0 +1,54 @@
+//
+//  File.swift
+//  
+//
+//  Created by Matt Greenfield on 22/4/24.
+//
+
+import Foundation
+
+// MARK: - Public
+
+public extension TimeInterval {
+    static func minutes(_ minutes: Int) -> TimeInterval { 60.0 * Double(minutes) }
+    static func hours(_ hours: Int) -> TimeInterval { .minutes(60) * Double(hours) }
+    static func days(_ days: Int) -> TimeInterval { .hours(24) * Double(days) }
+    var unit: Measurement<UnitDuration> { Measurement(value: self, unit: UnitDuration.seconds) }
+}
+
+public extension Date {
+    var age: TimeInterval { return -timeIntervalSinceNow }
+
+    func isToday(in calendar: Calendar = Calendar.current) -> Bool { calendar.isDateInToday(self) }
+    func isYesterday(in calendar: Calendar = Calendar.current) -> Bool { calendar.isDateInYesterday(self) }
+    func isTomorrow(in calendar: Calendar = Calendar.current) -> Bool { calendar.isDateInTomorrow(self) }
+
+    func startOfDay(in calendar: Calendar = Calendar.current) -> Date { calendar.startOfDay(for: self) }
+    func middleOfDay(in calendar: Calendar = Calendar.current) -> Date { startOfDay(in: calendar) + .hours(12) } // approximation
+    func endOfDay(in calendar: Calendar = Calendar.current) -> Date { nextDay(in: calendar).startOfDay(in: calendar) }
+
+    func sinceStartOfDay(in calendar: Calendar = Calendar.current) -> TimeInterval { timeIntervalSince(startOfDay(in: calendar)) }
+    func dayOfMonth(in calendar: Calendar = Calendar.current) -> Int { calendar.component(.day, from: self) }
+
+    func nextDay(in calendar: Calendar = Calendar.current) -> Date { calendar.nextDay(from: self) }
+    func previousDay(in calendar: Calendar = Calendar.current) -> Date { calendar.previousDay(from: self) }
+
+    func subtracting(days: Int, in calendar: Calendar = Calendar.current) -> Date { calendar.date(byAdding: .day, value: -days, to: self)! }
+    func adding(days: Int, in calendar: Calendar = Calendar.current) -> Date { calendar.date(byAdding: .day, value: days, to: self)! }
+
+    func isSameDayAs(_ date: Date, in calendar: Calendar = Calendar.current) -> Bool { calendar.isDate(date, inSameDayAs: self) }
+    func isSameMonthAs(_ date: Date, in calendar: Calendar = Calendar.current) -> Bool { calendar.isDate(date, equalTo: self, toGranularity: .month) }
+
+    static func -(lhs: Date, rhs: Date) -> TimeInterval { lhs.timeIntervalSince(rhs) }
+}
+
+public extension Calendar {
+    func previousDay(from date: Date) -> Date { self.date(byAdding: .day, value: -1, to: date)! }
+    func nextDay(from date: Date) -> Date { self.date(byAdding: .day, value: 1, to: date)! }
+}
+
+// MARK: - Internal
+
+extension DateInterval {
+    var range: ClosedRange<Date> { start...end }
+}
--- Sources/LocoKit2/Extensions/Foundation.swift
+++ Sources/LocoKit2/Extensions/Foundation.swift
@@ -4,12 +4,68 @@
 
 import Foundation
 
+// MARK: - Public
+
+public func withContinousObservation<T>(of value: @escaping @autoclosure () -> T, execute: @escaping (T) -> Void) {
+    withObservationTracking {
+        execute(value())
+    } onChange: {
+        Task { @MainActor in
+            withContinousObservation(of: value(), execute: execute)
+        }
+    }
+}
+
+public extension Array where Element: BinaryFloatingPoint {
+    func mean() -> Element {
+        guard !isEmpty else { return 0 }
+        return reduce(0, +) / Element(count)
+    }
+
+    func standardDeviation() -> Element {
+        guard count > 1 else { return 0 }
+        return meanAndStandardDeviation().standardDeviation
+    }
+
+    func meanAndStandardDeviation() -> (mean: Element, standardDeviation: Element) {
+        guard !isEmpty else { return (0, 0) }
+        let mean = self.mean()
+        guard count > 1 else { return (mean, 0) }
+        let sumOfSquaredDifferences = reduce(0) { $0 + (($1 - mean) * ($1 - mean)) }
+        let standardDeviation = sqrt(sumOfSquaredDifferences / Element(count - 1))
+        return (mean, standardDeviation)
+    }
+}
+
+public extension ProcessInfo {
+    // from Quinn the Eskimo at Apple
+    // https://forums.developer.apple.com/thread/105088#357415
+    var memoryFootprint: Measurement<UnitInformationStorage>? {
+        // The `TASK_VM_INFO_COUNT` and `TASK_VM_INFO_REV1_COUNT` macros are too
+        // complex for the Swift C importer, so we have to define them ourselves.
+        let TASK_VM_INFO_COUNT = mach_msg_type_number_t(MemoryLayout<task_vm_info_data_t>.size / MemoryLayout<integer_t>.size)
+        let TASK_VM_INFO_REV1_COUNT = mach_msg_type_number_t(MemoryLayout.offset(of: \task_vm_info_data_t.min_address)! / MemoryLayout<integer_t>.size)
+        var info = task_vm_info_data_t()
+        var count = TASK_VM_INFO_COUNT
+        let kr = withUnsafeMutablePointer(to: &info) { infoPtr in
+            infoPtr.withMemoryRebound(to: integer_t.self, capacity: Int(count)) { intPtr in
+                task_info(mach_task_self_, task_flavor_t(TASK_VM_INFO), intPtr, &count)
+            }
+        }
+        guard kr == KERN_SUCCESS, count >= TASK_VM_INFO_REV1_COUNT else { return nil }
+
+        return Measurement<UnitInformationStorage>(value: Double(info.phys_footprint), unit: .bytes)
+    }
+}
+
+// MARK: - Internal
+
 extension String {
     func deletingPrefix(_ prefix: String) -> String {
         guard self.hasPrefix(prefix) else { return self }
         return String(self.dropFirst(prefix.count))
     }
-    
+
     func appendLineTo(_ url: URL) throws {
         try (self + "\n").appendTo(url)
     }
@@ -31,91 +87,5 @@ extension Data {
         } else {
             try write(to: url, options: .atomic)
         }
-    }
-}
-
-extension TimeInterval {
-    static func minutes(_ minutes: Int) -> TimeInterval {
-        return 60.0 * Double(minutes)
-    }
-
-    static func hours(_ hours: Int) -> TimeInterval {
-        return .minutes(60) * Double(hours)
-    }
-
-    static func days(_ days: Int) -> TimeInterval {
-        return .hours(24) * Double(days)
-    }
-
-    var unit: Measurement<UnitDuration> {
-        return Measurement(value: self, unit: UnitDuration.seconds)
-    }
-}
-
-func withContinousObservation<T>(of value: @escaping @autoclosure () -> T, execute: @escaping (T) -> Void) {
-    withObservationTracking {
-        execute(value())
-    } onChange: {
-        Task { @MainActor in
-            withContinousObservation(of: value(), execute: execute)
-        }
-    }
-}
-
-extension Date {
-    var age: TimeInterval { return -timeIntervalSinceNow }
-
-    func isToday(in calendar: Calendar = Calendar.current) -> Bool { calendar.isDateInToday(self) }
-    func isYesterday(in calendar: Calendar = Calendar.current) -> Bool { calendar.isDateInYesterday(self) }
-    func isTomorrow(in calendar: Calendar = Calendar.current) -> Bool { calendar.isDateInTomorrow(self) }
-
-    func startOfDay(in calendar: Calendar = Calendar.current) -> Date { calendar.startOfDay(for: self) }
-    func middleOfDay(in calendar: Calendar = Calendar.current) -> Date { startOfDay(in: calendar) + .hours(12) } // approximation
-    func endOfDay(in calendar: Calendar = Calendar.current) -> Date { nextDay(in: calendar).startOfDay(in: calendar) }
-
-    func sinceStartOfDay(in calendar: Calendar = Calendar.current) -> TimeInterval { timeIntervalSince(startOfDay(in: calendar)) }
-    func dayOfMonth(in calendar: Calendar = Calendar.current) -> Int { calendar.component(.day, from: self) }
-
-    func nextDay(in calendar: Calendar = Calendar.current) -> Date { calendar.nextDay(from: self) }
-    func previousDay(in calendar: Calendar = Calendar.current) -> Date { calendar.previousDay(from: self) }
-
-    func subtracting(days: Int, in calendar: Calendar = Calendar.current) -> Date { calendar.date(byAdding: .day, value: -days, to: self)! }
-    func adding(days: Int, in calendar: Calendar = Calendar.current) -> Date { calendar.date(byAdding: .day, value: days, to: self)! }
-
-    func isSameDayAs(_ date: Date, in calendar: Calendar = Calendar.current) -> Bool { calendar.isDate(date, inSameDayAs: self) }
-    func isSameMonthAs(_ date: Date, in calendar: Calendar = Calendar.current) -> Bool { calendar.isDate(date, equalTo: self, toGranularity: .month) }
-
-    static func -(lhs: Date, rhs: Date) -> TimeInterval {
-        return lhs.timeIntervalSince(rhs)
-    }
-}
-
-extension Calendar {
-    func previousDay(from date: Date) -> Date { self.date(byAdding: .day, value: -1, to: date)! }
-    func nextDay(from date: Date) -> Date { self.date(byAdding: .day, value: 1, to: date)! }
-}
-
-extension DateInterval {
-    var range: ClosedRange<Date> { start...end }
-}
-
-extension Array where Element: BinaryFloatingPoint {
-    func mean() -> Element {
-        guard !isEmpty else { return 0 }
-        return reduce(0, +) / Element(count)
-    }
-
-    func standardDeviation() -> Element {
-        guard count > 1 else { return 0 }
-        return meanAndStandardDeviation().standardDeviation
-    }
-
-    func meanAndStandardDeviation() -> (mean: Element, standardDeviation: Element) {
-        guard !isEmpty else { return (0, 0) }
-        let mean = self.mean()
-        guard count > 1 else { return (mean, 0) }
-        let sumOfSquaredDifferences = reduce(0) { $0 + (($1 - mean) * ($1 - mean)) }
-        let standardDeviation = sqrt(sumOfSquaredDifferences / Element(count - 1))
-        return (mean, standardDeviation)
     }
 }
