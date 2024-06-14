2abbb79a6705c9a9e2bbfee5823f87c9171aa2cb
Matt <matt@bigpaua.com>
2024-06-14 10:18:49 +0800

    container for timeline item linked list management

---
+++ Sources/LocoKit2/Models/TimelineLinkedList.swift
@@ -0,0 +1,75 @@
+//
+//  TimelineLinkedList.swift
+//
+//
+//  Created by Matt Greenfield on 6/6/24.
+//
+
+import Foundation
+import GRDB
+
+public class TimelineLinkedList {
+
+    public private(set) var seedItem: TimelineItem
+    public private(set) var timelineItems: [String: TimelineItem] = [:]
+
+    public init?(fromItemId seedItemId: String) async {
+        do {
+            let seedItem = try await Database.pool.read {
+                try TimelineItemBase
+                    .including(optional: TimelineItemBase.visit)
+                    .including(optional: TimelineItemBase.trip)
+                    .including(all: TimelineItemBase.samples)
+                    .filter(Column("id") == seedItemId)
+                    .asRequest(of: TimelineItem.self)
+                    .fetchOne($0)
+            }
+            if let seedItem {
+                self.seedItem = seedItem
+                timelineItems[seedItem.id] = seedItem
+            } else {
+                return nil
+            }
+
+        } catch {
+            DebugLogger.logger.error(error, subsystem: .database)
+            return nil
+        }
+    }
+
+    public func previousItem(for item: TimelineItem) async -> TimelineItem? {
+        guard let previousItemId = item.base.previousItemId else { return nil }
+        return await getItem(itemId: previousItemId)
+    }
+
+    public func nextItem(for item: TimelineItem) async -> TimelineItem? {
+        guard let nextItemId = item.base.nextItemId else { return nil }
+        return await getItem(itemId: nextItemId)
+    }
+
+    // MARK: - Private
+
+    private func getItem(itemId: String) async -> TimelineItem? {
+        if let cached = timelineItems[itemId] { return cached }
+
+        do {
+            let item = try await Database.pool.read {
+                try TimelineItemBase
+                    .including(optional: TimelineItemBase.visit)
+                    .including(optional: TimelineItemBase.trip)
+                    .including(all: TimelineItemBase.samples)
+                    .filter(Column("id") == itemId)
+                    .asRequest(of: TimelineItem.self)
+                    .fetchOne($0)
+            }
+            guard let item else { return nil }
+            timelineItems[item.id] = item
+            return item
+
+        } catch {
+            DebugLogger.logger.error(error, subsystem: .database)
+            return nil
+        }
+    }
+
+}
