7258e3a086c91ffef271bfc8265fe782c0629ef1
Matt <matt@bigpaua.com>
2024-07-20 14:25:34 +0800

    enforce Swift 6 in package description

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
-// swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 6.0
 
 import PackageDescription
 
@@ -18,7 +17,16 @@ let package = Package(
         .package(name: "GRDB", url: "https://github.com/groue/GRDB.swift", from: "6.25.0"),
     ],
     targets: [
-        .target(name: "LocoKit2", dependencies: ["Surge", "GRDB", "Logging", "LoggingFormatAndPipe"]),
-        .testTarget(name: "LocoKit2Tests", dependencies: ["LocoKit2"])
-    ]
+        .target(
+            name: "LocoKit2", 
+            dependencies: ["Surge", "GRDB", "Logging", "LoggingFormatAndPipe"],
+            // why do i need this explicit flag when i've specified both tools version 6 and lang version 6
+            swiftSettings: [.enableExperimentalFeature("StrictConcurrency")]
+        ),
+        .testTarget(
+            name: "LocoKit2Tests",
+            dependencies: ["LocoKit2"]
+        )
+    ],
+    swiftLanguageVersions: [.version("6")]
 )
