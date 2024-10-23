b2a7ea7a7b440a8ac58e8591c6d52a2b7c6051d5
Matt <matt@bigpaua.com>
2024-10-23 15:00:29 +0800

    don’t really need custom executors. and scheduling is more efficient if you don’t use them

--- Sources/LocoKit2/TimelineActor.swift
+++ Sources/LocoKit2/TimelineActor.swift
@@ -10,13 +10,4 @@ import Foundation
 @globalActor
 public actor TimelineActor: GlobalActor {
     public static let shared = TimelineActor()
-
-    public static var queue: DispatchQueue { executor.queue }
-
-    private static let executor = CustomSerialExecutor(label: "TimelineActorQueue")
-
-    public static let sharedUnownedExecutor: UnownedSerialExecutor = TimelineActor.executor.asUnownedSerialExecutor()
-
-    nonisolated
-    public var unownedExecutor: UnownedSerialExecutor { Self.sharedUnownedExecutor }
 }
