56fc8fe4790b53dc7253fc68e14f27f6cd5611ca
Matt <matt@bigpaua.com>
2024-10-23 15:00:41 +0800

    getting ready for Place stuff

---
+++ Sources/LocoKit2/PlacesActor.swift
@@ -0,0 +1,13 @@
+//
+//  PlacesActor.swift
+//  LocoKit2
+//
+//  Created by Matt Greenfield on 2024-10-22.
+//
+
+import Foundation
+
+@globalActor
+public actor PlacesActor: GlobalActor {
+    public static let shared = PlacesActor()
+}
