b44b726d233413113b1f33202e8e43ce02951e11
Matt <matt@bigpaua.com>
2024-10-29 11:21:53 +0800

    uh, i hate commented out code, but … nice to have there for quick switching back on

--- Sources/LocoKit2/ActivityTypesActor.swift
+++ Sources/LocoKit2/ActivityTypesActor.swift
@@ -10,4 +10,13 @@ import Foundation
 @globalActor
 public actor ActivityTypesActor: GlobalActor {
     public static let shared = ActivityTypesActor()
+
+//    public static var queue: DispatchQueue { executor.queue }
+//
+//    private static let executor = CustomSerialExecutor(label: "ActivityTypeQueue")
+//
+//    public static let sharedUnownedExecutor: UnownedSerialExecutor = ActivityTypesActor.executor.asUnownedSerialExecutor()
+//
+//    nonisolated
+//    public var unownedExecutor: UnownedSerialExecutor { Self.sharedUnownedExecutor }
 }
--- Sources/LocoKit2/PlacesActor.swift
+++ Sources/LocoKit2/PlacesActor.swift
@@ -10,4 +10,13 @@ import Foundation
 @globalActor
 public actor PlacesActor: GlobalActor {
     public static let shared = PlacesActor()
+
+//    public static var queue: DispatchQueue { executor.queue }
+//
+//    private static let executor = CustomSerialExecutor(label: "PlacesActorQueue")
+//
+//    public static let sharedUnownedExecutor: UnownedSerialExecutor = PlacesActor.executor.asUnownedSerialExecutor()
+//
+//    nonisolated
+//    public var unownedExecutor: UnownedSerialExecutor { Self.sharedUnownedExecutor }
 }
