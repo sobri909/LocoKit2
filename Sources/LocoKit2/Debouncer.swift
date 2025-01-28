e3ed344c245b7979fa2710fb41f2e859dc82cd57
Matt <matt@bigpaua.com>
2025-01-28 17:03:29 +0900

    smarter debouncer

--- Sources/LocoKit2/Debouncer.swift
+++ Sources/LocoKit2/Debouncer.swift
@@ -1,26 +1,49 @@
 //
 //  Debouncer.swift
-//  Arc
 //
 //  Created by Matt Greenfield on 26/10/23.
-//  Copyright © 2023 Big Paua. All rights reserved.
 //
 
 import Foundation
 
-final class Debouncer {
-    private var currentTask: Task<Void, Error>? {
-        willSet { currentTask?.cancel() }
+final class Debouncer: Sendable {
+    private let worker = Worker()
+
+    func debounce(duration: TimeInterval, force forceInterval: TimeInterval = .infinity, _ action: @escaping @Sendable () async -> Void) {
+        Task { await worker.debounce(duration: duration, force: forceInterval, action) }
     }
 
-    deinit {
-        currentTask?.cancel()
+    func cancel() {
+        Task { await worker.cancel() }
     }
 
-    func debounce(duration: TimeInterval, _ action: @escaping @Sendable () async -> Void) {
-        currentTask = Task {
-            try await Task.sleep(for: .seconds(duration))
-            await action()
+    private actor Worker {
+        private var currentTask: Task<Void, Error>?
+        private var lastUpdateTime: Date?
+
+        deinit {
+            currentTask?.cancel()
+        }
+
+        func cancel() {
+            currentTask?.cancel()
+        }
+
+        func debounce(duration: TimeInterval, force forceInterval: TimeInterval = .infinity, _ action: @escaping @Sendable () async -> Void) {
+            if let lastUpdate = lastUpdateTime, lastUpdate.age > forceInterval {
+                lastUpdateTime = .now
+                currentTask?.cancel()
+                currentTask = Task { await action() }
+                return
+            }
+
+            // Otherwise normal debounce behavior
+            currentTask?.cancel()
+            currentTask = Task {
+                try await Task.sleep(for: .seconds(duration))
+                lastUpdateTime = .now
+                await action()
+            }
         }
     }
 }
