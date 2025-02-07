ccb2e0956e57286f4a8343b37ffa347f3731c5e7
Matt <matt@bigpaua.com>
2025-02-07 09:21:41 +0900

    require GRDB 7

--- Package.swift
+++ Package.swift
@@ -14,19 +14,10 @@ let package = Package(
         .package(url: "https://github.com/Jounce/Surge", from: "2.3.0"),
         .package(name: "Logging", url: "https://github.com/apple/swift-log", from: "1.5.4"),
         .package(name: "LoggingFormatAndPipe", url: "https://github.com/Adorkable/swift-log-format-and-pipe", from: "0.1.1"),
-        .package(name: "GRDB", url: "https://github.com/groue/GRDB.swift", from: "6.25.0")
+        .package(name: "GRDB", url: "https://github.com/groue/GRDB.swift", from: "7.0.0")
     ],
     targets: [
-        .target(
-            name: "LocoKit2", 
-            dependencies: ["Surge", "GRDB", "Logging", "LoggingFormatAndPipe"],
-            // why do i need this explicit flag when i've specified both tools version 6 and lang version 6
-            swiftSettings: [.enableExperimentalFeature("StrictConcurrency")]
-        ),
-        .testTarget(
-            name: "LocoKit2Tests",
-            dependencies: ["LocoKit2"]
-        )
-    ],
-    swiftLanguageVersions: [.version("6")]
+        .target(name: "LocoKit2", dependencies: ["Surge", "GRDB", "Logging", "LoggingFormatAndPipe"]),
+        .testTarget(name: "LocoKit2Tests", dependencies: ["LocoKit2"])
+    ]
 )
