59ad38ced31a55e3232758a87104c1e291835fd8
Matt <matt@bigpaua.com>
2025-03-26 16:09:40 +0700

    app layer can benefit from these. not a fan of them being public, but… this one time

--- Sources/LocoKit2/Extensions/GRDB+LocoKit.swift
+++ Sources/LocoKit2/Extensions/GRDB+LocoKit.swift
@@ -10,14 +10,14 @@ import GRDB
 
 extension DatabasePool {
     // perform a database read in a new top-level Task context to avoid cancellation
-    func uncancellableRead<T: Sendable>(_ operation: @escaping @Sendable (GRDB.Database) throws -> T) async throws -> T {
+    public func uncancellableRead<T: Sendable>(_ operation: @escaping @Sendable (GRDB.Database) throws -> T) async throws -> T {
         try await Task {
             try await self.read(operation)
         }.value
     }
     
     // perform a database write in a new top-level Task context to avoid cancellation
-    func uncancellableWrite<T: Sendable>(_ operation: @escaping @Sendable (GRDB.Database) throws -> T) async throws -> T {
+    public func uncancellableWrite<T: Sendable>(_ operation: @escaping @Sendable (GRDB.Database) throws -> T) async throws -> T {
         try await Task {
             try await self.write(operation)
         }.value
